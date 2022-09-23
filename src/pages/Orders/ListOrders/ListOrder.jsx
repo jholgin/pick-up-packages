db86d4f397f137580ab184d68e524ea7af1ca335
gcuriel02 <gcuriel@uninorte.edu.co>
2022-09-23 16:41:34 -0500

    tabla y header adjunto

--- src/pages/Orders/ListOrders/ListOrder.jsx
+++ src/pages/Orders/ListOrders/ListOrder.jsx
@@ -1,103 +1,77 @@
-import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import "./ListOrder.css";
 const ListOrder = () => {
   return (<>
-      <Button id="boton">Hola</Button>
-      <div class="table-responsive">
-        <table class="table table-striped table-sm">
+      {/* <Button id="boton">Hola</Button> */}
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Pick Up Packages</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#features">Órdenes</Nav.Link>
+            <Nav.Link href="#pricing">Login</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Breadcrumb>
+        <Breadcrumb.Item id="link_crear" href="#">Crear Orden</Breadcrumb.Item>
+      </Breadcrumb>
+      <div className="table-responsive">
+        <table className="table table-striped table-sm">
           <thead>
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
+              <th scope="col"># Servicio</th>
+              <th scope="col">Fecha</th>
+              <th scope="col">Ciudad Entrega</th>
+              <th scope="col">Dirección Entrega</th>
+              <th scope="col">Estado</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>1,001</td>
+              <td>1</td>
               <td>random</td>
               <td>data</td>
               <td>placeholder</td>
               <td>text</td>
             </tr>
             <tr>
-              <td>1,002</td>
+              <td>2</td>
               <td>placeholder</td>
               <td>irrelevant</td>
               <td>visual</td>
               <td>layout</td>
             </tr>
             <tr>
-              <td>1,003</td>
+              <td>3</td>
               <td>data</td>
               <td>rich</td>
               <td>dashboard</td>
               <td>tabular</td>
             </tr>
             <tr>
-              <td>1,003</td>
+              <td>4</td>
               <td>information</td>
               <td>placeholder</td>
               <td>illustrative</td>
               <td>data</td>
             </tr>
             <tr>
-              <td>1,004</td>
+              <td>5</td>
               <td>text</td>
               <td>random</td>
               <td>layout</td>
               <td>dashboard</td>
             </tr>
             <tr>
-              <td>1,005</td>
+              <td>6</td>
               <td>dashboard</td>
               <td>irrelevant</td>
               <td>text</td>
               <td>placeholder</td>
-            </tr>
-            <tr>
-              <td>1,006</td>
-              <td>dashboard</td>
-              <td>illustrative</td>
-              <td>rich</td>
-              <td>data</td>
-            </tr>
-            <tr>
-              <td>1,007</td>
-              <td>placeholder</td>
-              <td>tabular</td>
-              <td>information</td>
-              <td>irrelevant</td>
-            </tr>
-            <tr>
-              <td>1,008</td>
-              <td>random</td>
-              <td>data</td>
-              <td>placeholder</td>
-              <td>text</td>
-            </tr>
-            <tr>
-              <td>1,009</td>
-              <td>placeholder</td>
-              <td>irrelevant</td>
-              <td>visual</td>
-              <td>layout</td>
-            </tr>
-            <tr>
-              <td>1,010</td>
-              <td>data</td>
-              <td>rich</td>
-              <td>dashboard</td>
-              <td>tabular</td>
-            </tr>
-            <tr>
-              <td>1,011</td>
-              <td>information</td>
-              <td>placeholder</td>
-              <td>illustrative</td>
-              <td>data</td>
             </tr>
           </tbody>
         </table>
