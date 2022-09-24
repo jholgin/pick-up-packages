3099f8dc1159268eda69284c8ab388e617b7e3f1
juanmillan <j.millan@fastcodelab.com>
2022-09-23 21:58:21 -0500

    merge con rama sebastian

--- src/pages/Register/RegisterUser.jsx
+++ src/pages/Register/RegisterUser.jsx
@@ -1,47 +1,50 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const RegisterUser = () => {
   return (
-    <div style={{
-      display: 'flex',
-      width:'100%',
-      height:'100%', 
-      justifyContent:'center',
-      background:'#282626'
-    }}>
-    <div style={{ position: 'absolute',
-                  top: 150,
-                  margin: -25,
-                  width: 700, 
-                  padding: 30, 
-                  background:'black',
-                  color:'white',
-                  
-                  }}>
-    <h4>Registro</h4>
-    <Form>
-    <Form.Group>
-      <Form.Label>Ingrese su nombre:</Form.Label>
-      <Form.Control type="text" placeholder="Nombre Completo" />
-    </Form.Group>
-    <Form.Group>
-      <Form.Label>Ingrese su correo:</Form.Label>
-      <Form.Control type="email" placeholder="Correo Electronico" />
-    </Form.Group>
-    <Form.Group>
-      <Form.Label>Ingrese su contraseña:</Form.Label>
-      <Form.Control type="password" placeholder="Contraseña" />
-    </Form.Group>
-    <Button variant="primary" type="submit">
-      Submit
-    </Button>
-    </Form>
-    </div>
-    </div>
+    <>
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          background: "#282626",
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: 150,
+            margin: -25,
+            width: 700,
+            padding: 30,
+            background: "black",
+            color: "white",
+          }}
+        >
+          <h4>Registro</h4>
+          <Form>
+            <Form.Group>
+              <Form.Label>Ingrese su nombre:</Form.Label>
+              <Form.Control type="text" placeholder="Nombre Completo" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Ingrese su correo:</Form.Label>
+              <Form.Control type="email" placeholder="Correo Electronico" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Ingrese su contraseña:</Form.Label>
+              <Form.Control type="password" placeholder="Contraseña" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </>
   );
 };
 
