cc83b286e9389815f41a1315c5f142c9780725a1
nicolas <abrilcruznicolas@gmail.com>
2022-09-23 19:00:11 -0500

    Vista crear orden

--- src/pages/Orders/Create/CreateOrder.jsx
+++ src/pages/Orders/Create/CreateOrder.jsx
@@ -1,5 +1,35 @@
+import Card from 'react-bootstrap/Card';
+import NavigationBar from '../../../components/NavBarComponent/NavBarComponent';
+import CreateForm from '../../../components/CreateFormComponent.jsx/CreateFormComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './CreateOrder.css'
+
 const CreateOrder = () => {
-  return <div>CreateOrder</div>;
+  // const d = new Date();
+  // var datestring =d.getFullYear()+'-'+ d.getDate()  + "-" + (d.getMonth()+1);
+
+  return( 
+    <>
+    <div>
+      <NavigationBar />
+    </div>
+    <br/>
+    <div className='carta-contendedora'>
+    <Card >
+      <Card.Body>
+        <Card.Title>Ingrese datos de recogida y entrega</Card.Title>
+        <Card.Text>
+          <CreateForm/>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+      
+      
+      
+    </div>
+    
+    </>
+    );
 };
 
 export default CreateOrder;
